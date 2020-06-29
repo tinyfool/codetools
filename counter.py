86c14427f9bf8a93809a950e7eb7acf0a5d12723
Tinyfool <Tinyfool@gmail.com>
2020-06-29 22:21:31 +0800

    now we support git enterprise server

--- counter.py
+++ counter.py
@@ -1,12 +1,13 @@
+import config
 import requests
 
-token = ""
-apiHome = ""
+apiHome = config.apiHome
+token = "token " + config.token
 
 langDict = {}
 
-def checkOrg(org):
 
+def checkOrg(org):
     print(org["login"])
     print("========================")
     apiOrgrepos = apiHome + "/orgs/" + org["login"] + "/repos"
@@ -16,26 +17,41 @@ def checkOrg(org):
     for repo in repos:
         print(repo["full_name"])
         showlanguage(repo["languages_url"])
+    print()
+    print()
+
 
 def showlanguage(languages_url):
-
-
     response = requests.get(languages_url,
                             headers={"Authorization": token})
     langs = response.json()
     print(langs)
     for lang in langs:
-        langDict[lang] = langDict.get(lang,0) + langs[lang]
+        langDict[lang] = langDict.get(lang, 0) + langs[lang]
 
-apiAllorgs = apiHome + "/user/orgs"
-response = requests.get(apiAllorgs,
-                        headers={"Authorization": token})
-orgs = response.json()
-for org in orgs:
-    checkOrg(org)
 
-print(langDict)
-s=0
-for lang in langDict:
-    s = s + langDict[lang]
-print (s)
+if __name__ == "__main__":
+    if (apiHome == ""):
+        print("Please set api home at config.py")
+        exit()
+
+    if (token == ""):
+        print("Please set access token at config.py")
+        exit()
+
+    if config.serverType == "github":
+        apiAllorgs = apiHome + "/user/orgs"
+    else:
+        apiAllorgs = apiHome + "/organizations"
+
+    response = requests.get(apiAllorgs,
+                            headers={"Authorization": token})
+    orgs = response.json()
+    for org in orgs:
+        checkOrg(org)
+
+    print(langDict)
+    s = 0
+    for lang in langDict:
+        s = s + langDict[lang]
+    print(s)
